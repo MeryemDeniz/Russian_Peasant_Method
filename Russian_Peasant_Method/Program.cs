9d78c4781ef88a6cd017c7b7eabb5a9a11cd75fc
MeryemDeniz <meryemm.dnz@gmail.com>
2022-09-30 01:39:36 +0300

    Proje dosyası ekle.

---
+++ Russian_Peasant_Method/Program.cs
@@ -0,0 +1,34 @@
+﻿
+int firstNumber = GetNumberFromUser(1);
+int secondNumber = GetNumberFromUser(2);
+int result = 0;
+
+while (secondNumber >= 1)
+{
+    Console.WriteLine($"{firstNumber}  {secondNumber}", firstNumber);
+    if (secondNumber % 2 == 1)
+    {
+        result += firstNumber;
+    }
+    firstNumber *= 2;
+    secondNumber /= 2;
+}
+
+Console.WriteLine($"Sonuç : {result}");
+Console.ReadKey();
+
+
+int GetNumberFromUser(byte count)
+{
+    int number;
+    string value;
+
+    do
+    {
+        Console.Write($"{count}. Sayı : ");
+        value = Console.ReadLine();
+    } while (!int.TryParse(value, out number) || number < 1);
+
+
+    return number;
+}
